db0174f630ea93c8788e661cac19f327ac5d6210
James Rovel Barbosa <diagjames@gmail.com>
2021-05-13 14:25:06 -0300

    matrices ok

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MatrizExercicio {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n,n];
+
+            for (int i = 0; i < n; i++) { // percorrer linhas da matriz
+
+                string[] values = Console.ReadLine().Split(' '); // lendo a linha, recortando baseada no espaço em branco, e guardando no vetor values
+
+                for (int j = 0; j < n; j++) { // percorrer colunas da matriz
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Main Diagonal: ");
+            for (int i = 0; i < n; i++) {
+                Console.Write(mat[i,i] + " ");
+            }
+
+            Console.WriteLine();
+
+            int count = 0;
+            for ( int i = 0; i < n; i++) { // estrutura basica para percorrer matriz inteira
+
+                for (int j = 0; j < n; j++) {
+                    if (mat[i,j] < 0) {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Negative Numbers: " + count);
+
+        }
+    }
+}
